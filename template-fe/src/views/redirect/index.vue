553345fd569fd43202eb1d8dd1cc6fb7cf1b2248
罗建华 <jianhua.luo@ucarinc.com>
2020-12-29 18:20:44 +0800

    添加vue-element-admin后台框架

---
+++ template-fe/src/views/redirect/index.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  created() {
+    const { params, query } = this.$route
+    console.log(this.$route)
+    const { path } = params
+    console.log(this.$router)
+    this.$router.replace({ path: '/' + path, query })
+  },
+  render: function(h) {
+    return h() // avoid warning message
+  }
+}
+</script>
